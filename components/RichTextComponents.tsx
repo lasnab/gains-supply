9cc4265f42f1950059b95b97c6d72a8878ae6cb1
lasnab <hrithikbansal70@gmail.com>
2023-02-02 20:57:58 -0500

    fix blockquotes

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -24,6 +24,7 @@ export const RichTextComponents = {
     ),
   },
   block: {
+    normal: ({ children }: any) => <p className="pb-2">{children}</p>,
     h1: ({ children }: any) => (
       <h1 className="text-5xl py-10 font-bold">{children}</h1>
     ),
@@ -37,7 +38,7 @@ export const RichTextComponents = {
       <h4 className="text-2xl py-10 font-bold">{children}</h4>
     ),
     blockquote: ({ children }: any) => (
-      <blockquote className="border-l-black border-l-4 pl-5 py-5 my-5">
+      <blockquote className="border-l-black border-l-4 pl-5 py-5">
         {children}
       </blockquote>
     ),
